ecbf5cd7e9ae9534cb3a15f131dc8527d688ba45
Keyu Lai <kl2844@columbia.edu>
2016-03-01 16:47:43 -0500

    new frontend

--- frontend/map.js
+++ frontend/map.js
@@ -1,36 +1,62 @@
 function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 3,
-    styles: [{
-      featureType: 'poi',
-      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
-    }, {
-      featureType: 'transit.station',
-      stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
-    }],
-    disableDoubleClickZoom: true
+	center: {lat: 0, lng: 0},
+	zoom: 3,
+	styles: [{
+	  featureType: 'poi',
+	  stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+	}, {
+	  featureType: 'transit.station',
+	  stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+	}],
+	disableDoubleClickZoom: true
   });
 
   var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: [],
-    map: map,
-    radius: 18
+	data: [],
+	map: map,
+	radius: 18
   });
 
   map.addListener('click', function(e) {
-    var loc = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
-    var weight = Math.floor((Math.random() * 10) + 1);
+	var loc = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+	var weight = Math.floor((Math.random() * 10) + 1);
 
-    var arr = heatmap.getData();
+	var arr = heatmap.getData();
 
-    for (i = 0; i < weight; i++)
-      arr.push(loc);
+	for (i = 0; i < weight; i++)
+	  arr.push(loc);
   });
 }
 
-$("#SelectKeyWord").on("click", "li a", function() {
-    var platform = $(this).text();
-    alert(platform);
-});    
+var keyword = null;
+
+var xmlhttp = new XMLHttpRequest();
+var url = "http://127.0.0.1:5000/user/";
+xmlhttp.onreadystatechange = function() {
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		if (keyword == null)
+			alert("Please pick a keyword");
+		else
+			alert(xmlhttp.responseText);
+	}
+};
+
+$(".dropdown").on("click", "li a", function() {
+	keyword = $(this).text();
+	$(".dropdown-toggle").html(keyword + ' <span class="caret"></span>');
+}); 
+
+$('#dateselector1').datepicker("clearDates");
+$('#dateselector2').datepicker("clearDates");
+
+$("button").on("click", function() {
+	var beginDate = document.getElementById("dateselector1").value.split("/").join("-");
+	var endDate = document.getElementById("dateselector2").value.split("/").join("-");
+	xmlhttp.open("POST", url + "keyword=" + keyword + "&beginDate=" + beginDate + "&endDate=" + endDate, true);
+	xmlhttp.send();
+});
+
+
+
